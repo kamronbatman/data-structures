de75324a12635b8703c86a25bc975c1f3a5f366f
MKS-students <mks.students@hackreactor.com>
2015-07-01 19:25:29 -0700

    Completed sprint 1

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,29 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+
+  someInstance.storage = {};
+
+  someInstance.back = 0;
+  someInstance.front = 0;
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+	this.storage[this.back++] = value;
+};
 
+queueMethods.dequeue = function(){
+	return this.size() > 0 ? this.storage[this.front++] : undefined;
+};
 
+queueMethods.size = function(){
+	return this.back - this.front;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.length = 0;
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+	this.storage[this.length++] = value;
+};
 
+stackMethods.pop = function(){
+	return this.length > 0 ? this.storage[--this.length] : undefined;
+};
+
+stackMethods.size = function(){
+	return this.length;
+};
