a98729095312667b5222bc1c4859f187f373f28d
MKS-students <mks.students@hackreactor.com>
2015-07-01 19:37:51 -0700

    Completed LinkList (2nd time).

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if (list.head == null) { list.head = newNode; }
+    if (list.tail !== null ){ list.tail.next = newNode; }
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    if (list.head === null) { return undefined; }
+    var value = list.head.value;
+    list.head = list.head.next;
+
+    return value;
   };
 
   list.contains = function(target){
+    var node = list.head;
+
+    while (node !== null) {
+      if (node.value === target) { return true; }
+      node = node.next;
+    }
+
+    return false;
   };
 
   return list;
