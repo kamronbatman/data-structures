719a05219597ffc7ba704d37756815150d3326e1
MKS-students <mks.students@hackreactor.com>
2015-07-01 20:07:14 -0700

    Completed HashTable.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,27 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  this._storage[i] = this._storage[i] || [];
+  this.remove(k);
+  this._storage[i].push([k,v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  for(var idx = 0; idx < this._storage[i].length; idx++){
+    if(this._storage[i][idx][0] === k){
+      return this._storage[i][idx][1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = _.reject(this._storage[i], function(item){
+    return item[0] === k;
+  });
 };
 
 
