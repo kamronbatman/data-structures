a79c24706ec17d01c60dca8b3c3be41e58b46e31
MKS-students <mks.students@hackreactor.com>
2015-07-01 20:46:20 -0700

    Added larger test for depthFirstLog

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -37,4 +37,19 @@ describe('binarySearchTree', function() {
     console.log(array);
     expect(array).to.eql([5,2,3]);
   });
+
+  it('should execute a callback on every value in a tree using "depthFirstLog"', function(){
+    var array = [];
+    var func = function(value){ array.push(value); };
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(9);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(4);
+    binarySearchTree.depthFirstLog(func);
+    console.log(array);
+    expect(array).to.eql([5,8,9,7,2,3,4,1]);
+  });
 });
