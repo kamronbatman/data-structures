9acab8951ec7842d40b88c617f798edb76fab8af
MKS-students <mks.students@hackreactor.com>
2015-07-01 19:46:00 -0700

    Completed Tree (2nd time).

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,23 +3,25 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+  if (this.value === target) { return true; }
 
+  return _.reduce(this.children, function(found, child){
+    return found || child.contains(target);
+  }, false);
 };
 
 
